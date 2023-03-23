e8354137633fb5d5f0493acbf7c970c26bad70d8
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2023-03-23 13:51:42 +0200

    Register component added

---
+++ client/src/components/register/Register.js
@@ -0,0 +1,37 @@
+export default function Register() {
+    return (
+        <div className="register-page">
+            <form>
+                <h2>Create an account</h2>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" id="email" name="email" required="" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" required="" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" required="" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="repeat-password">Repeat Password</label>
+                    <input
+                        type="password"
+                        id="repeat-password"
+                        name="repeat-password"
+                        required=""
+                    />
+                </div>
+                <button type="submit">Register</button>
+                <p className="register-p">
+                    <span>You have an account?</span>{" "}
+                    <a href="#" className="hovered">
+                        Login
+                    </a>
+                </p>
+            </form>
+        </div>
+    );
+};
