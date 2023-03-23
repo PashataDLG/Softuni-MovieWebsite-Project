116d9c22382057fd7b3850d5dc2b27e865b5268a
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2023-03-23 14:01:31 +0200

    Details component added

---
+++ client/src/components/details/Details.js
@@ -0,0 +1,43 @@
+export default function Details() {
+    return (
+        <div className="movies-container">
+            <h1 className="movie-ctlg-h1">Movie Catalog</h1>
+            <div className="movie-catalog">
+                <img
+                    src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+                    alt="Movie poster"
+                />
+                <h2>Movie Title</h2>
+                <p>Movie description</p>
+                <button className="details-btn">Details</button>
+            </div>
+            <div className="movie-catalog">
+                <img
+                    src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+                    alt="Movie poster"
+                />
+                <h2>Movie Title</h2>
+                <p>Movie description</p>
+                <button className="details-btn">Details</button>
+            </div>
+            <div className="movie-catalog">
+                <img
+                    src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+                    alt="Movie poster"
+                />
+                <h2>Movie Title</h2>
+                <p>Movie description</p>
+                <button className="details-btn">Details</button>
+            </div>
+            <div className="movie-catalog">
+                <img
+                    src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+                    alt="Movie poster"
+                />
+                <h2>Movie Title</h2>
+                <p>Some of the very big descriptions evere</p>
+                <button className="details-btn">Details</button>
+            </div>
+        </div>
+    );
+}
