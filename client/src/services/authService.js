cb9dd433c805abd0467e26f0ce8b5cb07972f169
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2023-03-24 20:10:31 +0200

    auth service and reques function added

---
+++ client/src/services/authService.js
@@ -0,0 +1,29 @@
+import * as request from "../services/requester";
+
+const baseUrl = 'http://localhost:3030/users'
+
+export const login = async ({ email, password }) => {
+    const result = await request.post(`${baseUrl}/login`, { email, password });
+    return result;
+}
+
+
+export const register = async ({ email, password }) => {
+    const result = request.post(`${baseUrl}/register`, { email, password });
+    return result;
+}
+
+export const logout = async (accessToken) => {
+    try {
+        const res = await fetch(`${baseUrl}/logout`, {
+            method: 'GET',
+            headers: {
+                'X-authorization': accessToken
+            }
+        });
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+};
---
+++ client/src/services/requester.js
@@ -0,0 +1,56 @@
+const request = async (method, url, data) => {
+    try {
+        const user = localStorage.getItem('auth');
+        let auth = { accessToken: null };
+
+        if (user) {
+            try {
+                auth = JSON.parse(user);
+            } catch (err) {
+                console.error('Error parsing auth data: ', err);
+            }
+        }
+
+        let headers = {};
+
+        console.log('da')
+
+        if (auth.accessToken) {
+            headers['X-authorization'] = auth.accessToken
+        }
+
+        let buildRequest;
+
+        if (method === 'GET') {
+            buildRequest = fetch(url, { headers });
+        } else {
+            buildRequest = fetch(url, {
+                method,
+                headers: {
+                    ...headers,
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+        }
+        const response = await buildRequest;
+
+        if (response.ok !== true) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        const result = await response.json();
+
+        return result;
+    } catch (err) {
+        alert(err);
+        throw err;
+    }
+};
+
+export const get = request.bind({}, 'GET');
+export const put = request.bind({}, 'PUT');
+export const post = request.bind({}, 'POST');
+export const patch = request.bind({}, 'PATCH');
+export const del = request.bind({}, 'DELETE');
