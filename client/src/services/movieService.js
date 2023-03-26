729f721c8b2bb3b0f79ea6aa175fe18da578e447
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2023-03-26 17:45:04 +0300

    Movie service file formated

--- client/src/services/movieService.js
+++ client/src/services/movieService.js
@@ -1,6 +1,6 @@
 import * as request from './requester';
 
-const baseUrl = 'http://localhost:3030/data/movies'
+const baseUrl = 'http://localhost:3030/data/movies';
 
 export const getAll = () => request.get(baseUrl);
 
