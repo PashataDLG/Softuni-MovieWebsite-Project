adb3cc8831656c7fb83a87bd0b8630174f4b2e74
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2023-03-26 16:44:28 +0300

    Login component formated

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -15,23 +15,23 @@ export default function Login() {
             <form onSubmit={onSubmit}>
                 <h1>Login</h1>
                 <label htmlFor="username">Email</label>
-                <input 
-                type="text" 
-                id="email" 
-                name="email" 
-                placeholder="your_email@gmail.com"
-                value={values['email']}
-                onChange={changeHandler}
-                required
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="your_email@gmail.com"
+                    value={values['email']}
+                    onChange={changeHandler}
+                    required
                 />
                 <label htmlFor="password">Password</label>
-                <input 
-                type="password" 
-                id="password" 
-                name="password"
-                value={values['password']}
-                onChange={changeHandler}
-                required />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={values['password']}
+                    onChange={changeHandler}
+                    required />
                 <button type="submit">Login</button>
                 <p className="login-p">
                     <span>Don't have an account?</span>{" "}
